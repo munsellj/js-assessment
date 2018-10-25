53a9a7293fb34d68120499d9e35c65d97a2bfa1e
Jonathon Munsell <jm@chownow.com>
2018-10-25 11:32:44 -0400

    Added comments explaining array and object tests.

--- app/02-arrays-objects.js
+++ app/02-arrays-objects.js
@@ -1,24 +1,43 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
+
+  /**
+   * Return the number of times an item occurs in an array.
+   */
   count: function(arr, item) {
 
   },
 
+  /**
+   * Return an array with all instances of a received item removed.
+   */
   remove: function(arr, item) {
 
   },
 
+  /**
+   * Return an array containing only values which are duplicated in the
+   * recieved array.
+   */
   duplicates: function(arr) {
 
   }
 };
 
 exports.objectsAnswers = {
+
+  /**
+   * Alter the context in which the provided method runs.
+   */
   alterContext: function(fn, obj) {
 
   },
 
+  /**
+   * Return an array of key / value pairs in the "key: value" format of an
+   * object's "own" properties.
+   */
   iterate: function(obj) {
 
   }
